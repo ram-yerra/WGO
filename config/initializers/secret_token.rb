39c2232eb8a9f48adf5c5a734aaafd70dae2311e
yrkapil <yrkapil@gmail.com>
2012-08-01 13:09:51 +0530

    WGO Rails Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WgoRails::Application.config.secret_token = 'eb0b5eaa0c96a82fcc467991fd1b0590d8c0030df17f402d30751508537164f8d9dc6648baa6df9d48b36e52e23290aeca9a45377457f0d2707a67f53c7c1373'
