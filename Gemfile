e06acd8bcd7b9934bc6c517b81da3f68841841ca
yrkapil <yrkapil@gmail.com>
2012-08-07 11:10:55 +0530

    Added Users and its authentication

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 
+gem 'devise'
+
 group :production do
   gem 'pg'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WgoRails::Application.routes.draw do
+  devise_for :users
+
   resources :festivals
 
   # The priority is based upon order of creation:
